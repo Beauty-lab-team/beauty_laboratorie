e3cd462ede5b8b68942fff727645f0a53dc6bc60
Eduard Povierin <edpovierin@gmail.com>
2023-09-15 22:02:32 +0200

    remove comments

--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -1,22 +1,10 @@
 import Layout from '../components/Layout'
 import About from '../components/screens/about/About'
-// import HeroPost from '../components/hero-post'
-// import { getAllPostsForHome, getAllPostsFromServices } from '../lib/api'
 
 export default function AboutUs({ preview }) {
-   // const heroPost = nodes[0].posluga
-
    return (
       <Layout title='BEAUTY.LABORATORIE | Про нас' description='...'>
          <About />
       </Layout>
    )
 }
-
-// export const getStaticProps = async ({ preview = false }) => {
-//    const allPosts = await getAllPostsFromServices(preview)
-//    return {
-//       props: { allPosts, preview },
-//       revalidate: 10,
-//    }
-// }
