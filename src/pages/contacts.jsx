bb96548a8378ad459a08550f4632714cd7b31084
Eduard Povierin <edpovierin@gmail.com>
2023-09-16 15:31:19 +0200

    add contacts page

---
+++ src/pages/contacts.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/seo/Layout'
+
+import ContactsPage from '../components/screens/contacts/ContactsPage'
+
+export default function Contacts({ preview }) {
+   return (
+      <Layout title='BEAUTY.LABORATORIE | Контакти' description='...'>
+         <ContactsPage />
+      </Layout>
+   )
+}
