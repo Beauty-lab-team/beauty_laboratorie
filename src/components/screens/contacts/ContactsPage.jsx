d73bcde7f8c7f66e758b29dd20b559a73ba31432
Eduard Povierin <edpovierin@gmail.com>
2023-10-13 14:38:47 +0200

    add some contacts info

--- src/components/screens/contacts/ContactsPage.jsx
+++ src/components/screens/contacts/ContactsPage.jsx
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 import Section from '../../UI/Section'
 import Appointment from '../../common/appointment/Appointment'
-
+import Heading from '../../UI/Heading'
 export default function ContactsPage() {
    return (
       <Section className='pt-[140px] lg:pt-[160px]'>
+         <Heading>Як нас знайти</Heading>
+         <video className='w-full h-auto rounded-basic' src='/clinic/як-знайти.mp4' controls muted />
+         <Image className='mt-24 md:mt-44' src='/info.svg' width={600} height={600} alt='info' />
          <Appointment />
       </Section>
    )
