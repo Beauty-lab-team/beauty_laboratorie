639f0f92441073858d97cf4591a4946810d4bd5c
Eduard Povierin <edpovierin@gmail.com>
2024-01-09 00:15:31 +0100

    Improve motion animation

--- src/components/UI/modal/Modal.jsx
+++ src/components/UI/modal/Modal.jsx
@@ -1,12 +1,23 @@
+'use client'
 import s from './Modal.module.scss'
+import { motion } from 'framer-motion'
 
 export default function Modal({ open, children, handleClose }) {
    return (
-      <div className={`${s.modalBlock} ${open ? s.show : ''}`}>
+      <motion.div
+         className={s.modalBlock}
+         initial={{ opacity: 0, scale: 0.8 }}
+         animate={open ? { opacity: 1, scale: 1, pointerEvents: 'auto' } : {}}
+         transition={{
+            duration: 0.3,
+            type: 'spring',
+            ease: 'easeOut',
+         }}
+      >
          <div className={s.backdrop} onClick={() => handleClose(false)}></div>
-         <div className={s.modal}>
+         <div className={`${s.modal} ${open ? s.open : ''}`}>
             <div className={s.content}>{children}</div>
          </div>
-      </div>
+      </motion.div>
    )
 }
