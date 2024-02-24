773dfa9a31c3dbb0aee0681cf9251da6ad6a22a0
Eduard Povierin <edpovierin@gmail.com>
2024-02-24 13:54:02 +0100

    Some design fixes

--- src/components/UI/Text.jsx
+++ src/components/UI/Text.jsx
@@ -1,6 +1,6 @@
 export default function Text(props) {
    return (
-      <p {...props} className={`text-text-2 text-lg leading-[28px] overflow-hidden ${props.className}`}>
+      <p {...props} className={`text-text-1/90 text-lg leading-[28px] overflow-hidden ${props.className}`}>
          {props.children}
       </p>
    )
