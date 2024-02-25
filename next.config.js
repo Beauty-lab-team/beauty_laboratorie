6326357e6384dc74c535630beb480a61c460a3bd
Eduard Povierin <edpovierin@gmail.com>
2024-02-25 17:47:55 +0100

    Change token

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
    },
    env: {
       TOKEN: '6636841061:AAF7wnWZbg5DDsvnd79KX7v9wBnw_GCRq4o',
-      CHAT_ID: '573909735',
+      // CHAT_ID: '573909735',
+      CHAT_ID: '979872254',
    },
 }
