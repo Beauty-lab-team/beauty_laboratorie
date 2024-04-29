3222ecdca13652ba8c8dc10450ae164acacbc428
Eduard Povierin <edpovieri@gmail.com>
2024-04-29 16:05:26 +0200

    Fix import

--- src/components/common/form/Form.jsx
+++ src/components/common/form/Form.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import ReactInputMask from 'react-input-mask'
 import s from './Form.module.scss'
 import Button from '../../UI/Button'
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/router'
 
 export default function Form({ handleClose }) {
    const router = useRouter()
