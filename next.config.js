bb27511eae281b56095aaf00bd4754f9987ecf7e
Eduard Povierin <edpovieri@gmail.com>
2024-04-29 16:01:54 +0200

    Update .env

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ module.exports = {
    images: {
       domains: ['beautylabstorage.blob.core.windows.net'],
    },
-   env: {
-      TOKEN: '6636841061:AAF7wnWZbg5DDsvnd79KX7v9wBnw_GCRq4o',
-      CHAT_ID: '573909735',
-      INSTA: '36902eb00aa43f07ee81ce5a04e3a575',
-   },
 }
