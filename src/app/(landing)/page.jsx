38402e9045a85764ab93815a92a3cd6ccf4924c5
Eduard Povierin <edpovieri@gmail.com>
2024-05-03 10:13:50 +0200

    Fix design

--- src/app/(landing)/page.jsx
+++ src/app/(landing)/page.jsx
@@ -3,7 +3,7 @@ import Benefits from '../../components/screens/home/benefits/Benefits'
 import Laser from '../../components/screens/home/laser/Laser'
 import ServicesHome from '../../components/screens/home/services/ServicesHome'
 import Welcome from '../../components/screens/home/welcome/Welcome'
-import About from './about-us/page'
+import About from '../../components/screens/home/about/About'
 import LandingLayout from './layout'
 
 export default function Home() {
--- src/components/common/employees/Employee.jsx
+++ src/components/common/employees/Employee.jsx
@@ -7,7 +7,7 @@ export default function Employee({ name, about, profile, image }) {
          <Image className={s.image} alt={name} src={image} width={300} height={370} quality={100} />
          <p className={s.name}>{name}</p>
          <p className={s.about}>{about}</p>
-         <p className={s.profileTitle}>Основна професійна спеціалізація:</p>
+         {/* <p className={s.profileTitle}>Основна професійна спеціалізація:</p> */}
          <ul className={s.profile}>
             {profile.map((el, i) => (
                <li key={i}>{el}</li>
