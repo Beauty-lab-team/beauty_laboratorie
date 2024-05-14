287c865604369ab1e14113e5bd20a8aa7eaed5c3
Eduard Povierin <edpovieri@gmail.com>
2024-05-14 14:14:55 +0200

    Add new article and improve design

--- src/app/(landing)/services/[service]/page.jsx
+++ src/app/(landing)/services/[service]/page.jsx
@@ -4,6 +4,7 @@ import { data } from '../../../../data.js'
 import { useEffect, useState } from 'react'
 import Text from '../../../../components/UI/Text'
 import Heading from '../../../../components/UI/Heading'
+import Section from '../../../../components/UI/Section'
 
 export default function Service({ params }) {
    const { service } = params
@@ -24,12 +25,12 @@ export default function Service({ params }) {
    }, [])
 
    return (
-      <div>
-         <Heading className='mt-20 text-4xl'>{categoryMain}</Heading>
+      <Section className='pt-[140px] lg:pt-[160px] max-w-2xl mx-auto'>
+         <Heading>{categoryMain}</Heading>
          <Text
-            className='prose prose-headings:text-text-1 prose-li:marker:text-text-2 prose-strong:text-text-1 prose-h1:mt-12 prose-h1:text-4xl'
+            className='prose prose-headings:text-text-1 prose-li:marker:text-text-2 prose-strong:text-text-1 prose-h1:mt-12 prose-h1:text-4xl prose-video:rounded-basic prose-img:rounded-basic prose-video:max-w-lg prose-img:max-w-lg'
             dangerouslySetInnerHTML={{ __html: content }}
          ></Text>
-      </div>
+      </Section>
    )
 }
