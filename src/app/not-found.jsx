f1a9b7d5586530d55e48a1507db4b24194e89fef
Eduard Povierin <edpovieri@gmail.com>
2024-07-03 18:19:53 +0200

    Fix build errors (provide data props)

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,13 +1,25 @@
-import Footer from '../components/footer/Footer'
+import { load } from 'outstatic/server'
 import Navbar from '../components/navbar/Navbar'
 
-export default function notFound() {
+export default async function notFound() {
+   const { allServices } = await getData()
+
    return (
       <div>
-         <Navbar />
+         <Navbar servicesLinks={allServices} />
          <main className='container max-w-5xl pt-24 md:pt-44 px-3 md:px-12 flex-1 mx-auto text-text-1 text-3xl'>
             Будь ласка, скористайся пунктами меню для пошуку потрібної інформації. <br /> <br /> Дякуємо тобі за терпіння 🌸
          </main>
       </div>
    )
+}
+
+async function getData() {
+   const db = await load()
+
+   const allServices = await db.find({ collection: 'services' }, ['title', 'slug', 'content']).sort({ publishedAt: -1 }).toArray()
+
+   return {
+      allServices,
+   }
 }
--- src/components/screens/about/team/Team.jsx
+++ src/components/screens/about/team/Team.jsx
@@ -1,12 +1,27 @@
+import { load } from 'outstatic/server'
 import Heading from '../../../UI/Heading'
 import Section from '../../../UI/Section'
 import Employees from '../../../common/employees/Employees'
 
-export default function Team() {
+export default async function Team() {
+   const { allEmployees } = await getData()
+
    return (
       <Section>
          <Heading>Нашi спецiалiсти</Heading>
-         <Employees />
+         <Employees allEmployees={allEmployees} />
       </Section>
    )
+}
+
+async function getData() {
+   const db = await load()
+
+   const allEmployees = (
+      await db.find({ collection: 'employee' }, ['title', 'content', 'coverImage', 'skill']).sort({ publishedAt: -1 }).toArray()
+   ).reverse()
+
+   return {
+      allEmployees,
+   }
 }
