51de0a4ce99350dd66071b77aa1b42411bd4c478
Eduard Povierin <67825832+NightWanderer2004@users.noreply.github.com>
2024-08-08 15:38:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
    images: {
       domains: ['beautylabstorage.blob.core.windows.net'],
    },
+   experimental: {
+       outputStandalone: true,
+   },
    swcMinify: false,
    reactStrictMode: false,
    webpack: config => {
