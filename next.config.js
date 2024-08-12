7e4601bacf3eb80f3c7d94d70c5bbe93a0fdc943
Eduard Povierin <edpovieri@gmail.com>
2024-08-12 15:59:41 +0200

    Optimize packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-   images: {
-      domains: ['beautylabstorage.blob.core.windows.net'],
-   },
    experimental: {
-       outputStandalone: true,
+      outputStandalone: true,
    },
    swcMinify: false,
    reactStrictMode: false,
