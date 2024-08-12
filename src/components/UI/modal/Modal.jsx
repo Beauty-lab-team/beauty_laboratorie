c3725c6275c443678c4bbadeb76ea14cb5c0f0ff
Eduard Povierin <edpovieri@gmail.com>
2024-08-12 17:09:58 +0200

    Remove framer

--- src/components/UI/modal/Modal.jsx
+++ src/components/UI/modal/Modal.jsx
@@ -1,25 +1,13 @@
 'use client'
 import s from './Modal.module.scss'
-import { motion } from 'framer-motion'
 
 export default function Modal({ open, children, handleClose }) {
    return (
-      <motion.div
-         className={s.modalBlock}
-         initial={{ opacity: 0, scale: 0.9, pointerEvents: 'none' }}
-         animate={open ? { opacity: 1, scale: 1, pointerEvents: 'auto' } : {}}
-         transition={{
-            duration: 0.3,
-            type: 'spring',
-            ease: 'easeOut',
-            stiffness: 500,
-            damping: 30,
-         }}
-      >
+      <div className={s.modalBlock + ' ' + (open ? s.open : '')}>
          <div className={s.backdrop} onClick={() => handleClose(false)}></div>
          <div className={`${s.modal} ${open ? s.open : ''}`}>
             <div className={s.content}>{children}</div>
          </div>
-      </motion.div>
+      </div>
    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -30,7 +30,7 @@ module.exports = {
          },
       },
    },
-   plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/typography'), require('daisyui')],
+   plugins: [require('@tailwindcss/typography'), require('daisyui')],
    daisyui: {
       styled: false,
    },
