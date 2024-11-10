9c83cb1278731c77ac0229dd22b07c10f89d521e
Eduard Povierin <edpovieri@gmail.com>
2024-11-10 18:27:23 +0100

    Update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -12,5 +12,83 @@ export default function sitemap() {
          changeFrequency: 'monthly',
          priority: 0.8,
       },
+      {
+         url: 'https://cosmetcab.dp.ua/about-us',
+         lastModified: new Date(),
+         changeFrequency: 'yearly',
+         priority: 0.9,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.8,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services/Lazerna-epilyatsiya-v-Dnipri',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.7,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services/Slimsfera-v-Dnipri',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.7,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services/Presoterapiya-v-Dnipri',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.7,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services/Mikroholkovyy-RF-v-Dnipri',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.7,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services/Fitobochka-v-Dnipri',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.7,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/services/Smas-lifting-v-Dnipri',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.7,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/contacts',
+         lastModified: new Date(),
+         changeFrequency: 'yearly',
+         priority: 0.9,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/actual',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.8,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/promotions',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.8,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/articles',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.8,
+      },
+      {
+         url: 'https://cosmetcab.dp.ua/news',
+         lastModified: new Date(),
+         changeFrequency: 'monthly',
+         priority: 0.8,
+      },
    ]
 }
