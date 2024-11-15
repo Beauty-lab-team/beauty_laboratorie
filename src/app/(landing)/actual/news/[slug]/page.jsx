2a4c95780278314b76200b21bf1e3dd21267635e
Eduard Povierin <edpovieri@gmail.com>
2024-11-15 13:27:33 +0100

    Return back

--- src/app/(landing)/actual/news/[slug]/page.jsx
+++ src/app/(landing)/actual/news/[slug]/page.jsx
@@ -21,30 +21,23 @@ export async function generateMetadata(params) {
 }
 
 export default async function News(params) {
-   try {
-      const news = await getData(params)
-      if (Object.keys(news).length == 0) redirect('/')
+   const news = await getData(params)
 
-      const formattedDate = format(new Date(news.publishedAt), 'dd MMMM yyyy', { locale: ukLocale })
+   const formattedDate = format(new Date(news.publishedAt), 'dd MMMM yyyy', { locale: ukLocale })
 
-      return (
-         <Section className='pt-[140px] lg:pt-[160px] max-w-5xl mx-auto'>
-            <Heading className='tracking-normal'>{news.title}</Heading>
-            <Text
-               className='prose mb-10 max-w-5xl prose-headings:text-text-1 prose-li:marker:text-text-2 prose-strong:text-text-1 prose-a:text-text-1 prose-a:underline prose-h1:mt-12 prose-h1:text-4xl prose-video:rounded-basic prose-img:rounded-basic prose-video:max-w-2xl prose-img:max-w-2xl prose-video:w-full prose-img:w-full'
-               dangerouslySetInnerHTML={{ __html: news.content }}
-            ></Text>
-            <Text className='italic mb-3'>{formattedDate}</Text>
-            <LinkToPage className='w-full lg:w-auto block lg:inline-block text-center' link='/'>
-               На головну
-            </LinkToPage>
-         </Section>
-      )
-   } catch (error) {
-      // Log the error and redirect to the homepage
-      console.error(`Error fetching news data: ${error}`)
-      redirect('/')
-   }
+   return (
+      <Section className='pt-[140px] lg:pt-[160px] max-w-5xl mx-auto'>
+         <Heading className='tracking-normal'>{news.title}</Heading>
+         <Text
+            className='prose mb-10 max-w-5xl prose-headings:text-text-1 prose-li:marker:text-text-2 prose-strong:text-text-1 prose-a:text-text-1 prose-a:underline prose-h1:mt-12 prose-h1:text-4xl prose-video:rounded-basic prose-img:rounded-basic prose-video:max-w-2xl prose-img:max-w-2xl prose-video:w-full prose-img:w-full'
+            dangerouslySetInnerHTML={{ __html: news.content }}
+         ></Text>
+         <Text className='italic mb-3'>{formattedDate}</Text>
+         <LinkToPage className='w-full lg:w-auto block lg:inline-block text-center' link='/'>
+            На головну
+         </LinkToPage>
+      </Section>
+   )
 }
 
 async function getData({ params }) {
@@ -53,9 +46,7 @@ async function getData({ params }) {
       .find({ collection: 'news', slug: params.slug.toLowerCase() }, ['title', 'publishedAt', 'description', 'slug', 'content', 'coverImage'])
       .first()
 
-   if (!news) {
-      return {}
-   }
+   if (!news) redirect('/')
 
    const converter = new Showdown.Converter()
    const content = converter.makeHtml(news.content)
